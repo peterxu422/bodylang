f79e1c066cac76a34f49c3bb065cd940573a2bc3
Peter Xu <peterxu422@gmail.com>
2014-01-19 17:46:47 -0500

    search by body part basic implementation

--- getexpr.php
+++ getexpr.php
@@ -2,20 +2,25 @@
     //Echo statements cannot be seen if PHP script placed here. Need to place it in the div home in order to see it.
     //echo \"Hello Php\";
     
-    $con=mysqli_connect("localhost", "root", "usbw", "movedb");
+    if(isset($_GET["bodypart"])) {
+        //echo "Is set <br/>" . $_GET["bodypart"] . " <br/>" . $_REQUEST["bodypart"] . "<br/>";   
+        $qry = "WHERE Keyword='" . strtolower($_GET["bodypart"]) . "'";
+    }
+    else {
+        echo "Is Not Set<br/>";
+    }
 
+    $con=mysqli_connect("localhost", "root", "usbw", "movedb");
     if(mysqli_connect_errno()) {
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
     }
 
-    $result = mysqli_query($con, "SELECT * FROM tblexpressionsforwebtesting");
-    
+    $result = mysqli_query($con, "SELECT * FROM tblexpressionsforwebtesting " . $qry);
+
     while($row = mysqli_fetch_array($result)) {
         echo "<b>" . $row['Expression'] . "</b>" . ": " . $row['Definition'] . "<br/>Example: " . $row['Example'];
         echo "<br/><br/>";
-        
     }
-
-    mysqli_close($con);
     
+    mysqli_close($con);
 ?>
--- script.js
+++ script.js
@@ -1,24 +1,9 @@
 
 
-
+/*
 $(document).ready(function() {
     "use strict";
     //Echo statements cannot be seen if PHP script placed outside 'div home'. Need to place it in the 'div home' in order to see it.
-    
-    /*
-    var phpstring = "<?php                                                                                                              \
-            $con=mysqli_connect(\"localhost\", \"root\", \"usbw\", \"movedb\");                                                         \
-            if(mysqli_connect_errno()) {                                                                                                \
-                echo \"Failed to connect to MySQL: \" . mysqli_connect_error();                                                         \
-            }                                                                                                                           \
-            $result = mysqli_query($con, \"SELECT * FROM tblexpressionsforwebtesting\");                                                \
-            while($row = mysqli_fetch_array($result)) {                                                                                 \
-                echo \"<b>\" . $row[\'Expression\'] . \"</b>\" . \": \" . $row[\'Definition\'] . \"<br/>Example:\" . $row[\'Example\']; \
-                echo \"<br/><br/>\";                                                                                                    \
-            }                                                                                                                           \
-            mysqli_close($con);                                                                                                         \
-        ?>";
-    */
     
     $("#submit_btn").click(function() {
         //alert("Submit search query");
@@ -32,10 +17,12 @@ $(document).ready(function() {
         getexpr();
     });
 });
+*/
 
-
-function getexpr() {
+function getexpr(str) {
+    //Changes the page in the app
     document.location.href="#results";
+    
     if(window.XMLHttpRequest) {
         xmlhttp = new XMLHttpRequest();   
     }
@@ -46,10 +33,14 @@ function getexpr() {
     xmlhttp.onreadystatechange=function() {
         if(xmlhttp.readyState==4 && xmlhttp.status==200) {
             $("#res_content").html(xmlhttp.responseText);
-            
         }
     }
-            
-    xmlhttp.open("GET", "getexpr.php", true);
+    
+    var expr = ($("#expression").val() == null) ? "" : $("#expression").val();
+    var feeling = ($("#feelings").val() == null) ? "" : $("#feelings").val();
+    var bodypart = ($("#bodyPart").val() == null) ? "" : $("#bodyPart").val();
+    var url = "?expression="+expr+"&feelings="+feeling+"&bodypart="+bodypart;
+    
+    xmlhttp.open("GET", "getexpr.php"+url, true);
     xmlhttp.send();
 }
