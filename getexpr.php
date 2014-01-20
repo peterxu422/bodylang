af9af3dc73e4bf3ec9862445b9992e054f7529f7
Peter Xu <peterxu422@gmail.com>
2014-01-20 18:41:50 -0500

    implemented bodypart and feelings searches

--- getexpr.php
+++ getexpr.php
@@ -4,21 +4,49 @@
     
     if(isset($_GET["bodypart"])) {
         //echo "Is set <br/>" . $_GET["bodypart"] . " <br/>" . $_REQUEST["bodypart"] . "<br/>";   
-        $qry = "WHERE Keyword='" . strtolower($_GET["bodypart"]) . "'";
+        if($_GET["bodypart"] == "Body Part") {
+            $qryBodyPart = "";   
+        }
+        else {
+            $qryBodyPart = "`Keyword`='" . strtolower($_GET["bodypart"]) . "'";
+        }
+    }
+    
+    if(isset($_GET["feelings"])) {
+        if($_GET["feelings"] == "Feeling") {
+            $qryFeelings = "";
+        }
+        else {
+            $qryFeelings = "`Describe`='" . $_GET["feelings"] . "'";   
+        }
+    }
+    
+    $qry = "SELECT * FROM `tblexpressionsforwebtesting`";
+    if($qryBodyPart == "") {
+        if($qryFeelings != "") {
+            $qry .= " WHERE " . $qryFeelings;
+        }    
     }
     else {
-        echo "Is Not Set<br/>";
+        $qry .= " WHERE " . $qryBodyPart;
+        
+        if($qryFeelings != "") {
+            $qry .= " AND " . $qryFeelings;
+        }
+        
     }
+
+    echo $qry . "<br/>";
 
     $con=mysqli_connect("localhost", "root", "usbw", "movedb");
     if(mysqli_connect_errno()) {
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
     }
 
-    $result = mysqli_query($con, "SELECT * FROM tblexpressionsforwebtesting " . $qry);
+    $result = mysqli_query($con, $qry);
 
     while($row = mysqli_fetch_array($result)) {
-        echo "<b>" . $row['Expression'] . "</b>" . ": " . $row['Definition'] . "<br/>Example: " . $row['Example'];
+        echo "<b>" . $row['Expression'] . "</b>" . ": " . $row['Definition'] . "<br/>Example: " . $row['Example'] ;
         echo "<br/><br/>";
     }
     
